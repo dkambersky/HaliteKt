e8e0dc1f3cd63c28f61aa0fee73c487cb12eba39
David Kamberský <david.kambersky@gmail.com>
2017-01-15 21:41:51 +0000

    OVERKILL WORKING.

--- KotlinBot/src/MyBot.kt
+++ KotlinBot/src/MyBot.kt
@@ -1,10 +1,9 @@
-import com.sun.org.apache.xml.internal.security.Init
 import java.io.IOException
 import java.util.logging.FileHandler
 import java.util.logging.Level
 import java.util.logging.Logger
 import java.util.logging.SimpleFormatter
-import javax.print.attribute.IntegerSyntax
+
 
 /**
  * Created by David on 1
@@ -38,41 +37,43 @@ val NEAREST_DIR_SEARCH_FACTOR = 1
 
 
 /* Logging */
-val LOGGING_LEVEL = Level.INFO
-val BOT_NAME = "DavKotlinBot_v1"
-val LOGFILE_PREFIX = "../logs/"
-val LOGFILE_SUFFIX = ".txt"
+val LOGGING_LEVEL: Level = Level.INFO
+const val BOT_NAME = "DavKotlinBot_v1"
+const val LOGFILE_PREFIX = "../logs/"
+const val LOGFILE_SUFFIX = ".txt"
 
 /* Map Processing */
 
 /*
  * should be a multiple of 5 because of conventional map dimensions
  */
-val SECTORS_BY_SIDE = 10;
+val SECTORS_BY_SIDE = 10
 
 /*
  * size of the edge of the blur applied
  */
-val  BLUR_DIMENSION = 3;
+val  BLUR_DIMENSION = 3
 
 /*
  * 0 desirability | 1 enemy army strength | 2 friendly power arriving
  */
-val PROCESSING_PROPERTIES = 3;
+val PROCESSING_PROPERTIES = 3
 
 
 /* Globals */
 var myId = 0
 var turnCounter = 0
+var gameMap:GameMap? = null
 
 /* Algorithm stuff */
-var processedMap = arrayOf(Float,Float,Float)
-var qualityMap = arrayOf(Float,Float,Float)
-var regionMap = arrayOf(Float,Float,Float)
+var processedMap:Array<Array<FloatArray>>? = null
+var qualityMap:Array<Array<FloatArray>>? = null
+var regionMap:Array<Array<FloatArray>>? = null
 
 /* Logging */
 val logger:Logger = Logger.getLogger(BOT_NAME)
 var handler:FileHandler? = null
+var turnTimes:MutableList<Int>?=null
 
 
 fun main(args: Array<String>) {
@@ -84,7 +85,8 @@ fun main(args: Array<String>) {
 
     val iPackage = Networking.getInit()
     myId = iPackage.myID
-    val gameMap = iPackage.map
+    gameMap = iPackage.map
+    val gameMap = gameMap!!
 
     initMap()
 
@@ -94,6 +96,9 @@ fun main(args: Array<String>) {
 
 
     while (true) {
+        turnCounter++
+        val turnStart = System.currentTimeMillis()
+
         val moves = mutableListOf<Move>()
         Networking.updateFrame(gameMap)
 
@@ -102,15 +107,259 @@ fun main(args: Array<String>) {
                 val location = gameMap.getLocation(x, y)
                 val site = location.site
 
-                if (site.owner == myId) moves.add(Move(location, Direction.randomDirection()))
+                if (site.owner == myId) moves.add(Move(location, nextMove(location)))
             }
         }
+        logTurn(turnStart)
         Networking.sendFrame(moves)
 }
 }
 
-fun initMap() {
+fun nearestEnemyDir(loc: Location): Direction {
+    val gameMap = gameMap!!
+    var dir = Direction.STILL
 
+
+    var maxDistance = Math.min(gameMap.width, gameMap.height) / NEAREST_DIR_SEARCH_FACTOR
+
+    for (d in Direction.CARDINALS) {
+        var distance = 0
+        var current = loc
+        var site = gameMap.getSite(loc)
+
+        while (site.owner == myId && distance < maxDistance) {
+            distance++
+            current = gameMap.getLocation(current, d)
+            site = gameMap.getSite(current)
+        }
+
+        if (distance < maxDistance) {
+            dir = d
+            maxDistance = distance
+        }
+    }
+
+    if (dir == Direction.STILL)
+        logger.warning(String.format(
+                "Turn %d: [%d,%d] nearestEnemyDir returned STILL! ",
+                turnCounter, loc.x, loc.y))
+
+    return dir
+}
+
+fun heuristic(loc: Location): Float {
+    val gameMap = gameMap!!
+    val site = gameMap.getSite(loc)
+
+    logger.fine("Owner of tile: " + site.owner)
+
+    /* If neutral, return simple heuristic */
+    if (site.owner == 0 && site.strength > 0) {
+
+
+        val heuristic = site.production.toFloat() / site.strength.toFloat()
+        return heuristic
+    }
+
+    /* If enemy, go by overkill damage */
+    var totalDamage = 0
+    for (dir in Direction.CARDINALS) {
+        val neighbour = gameMap.getSite(loc, dir)
+        if (neighbour.owner != 0 && neighbour.owner != myId)
+            totalDamage += neighbour.strength
+    }
+
+    logger.fine(String.format(
+            "Player tile: [%d, %d] prod: %d str: %d. damage: %d", loc.x,
+            loc.y, site.production, site.strength, totalDamage))
+    return totalDamage.toFloat()
+
+}
+
+fun nextMove(loc: Location): Direction {
+    val gameMap = gameMap!!
+    val site = gameMap.getSite(loc)
+
+
+    var border = false
+    logger.fine("Calculating a move.")
+
+    /* Detect borders */
+    var highestHeurDir = Direction.STILL
+
+    for (dir in Direction.CARDINALS) {
+        val neighLoc = gameMap.getLocation(loc, dir)
+        val neighbor = gameMap.getSite(neighLoc)
+
+        if (neighbor.owner != myId) {
+            border = true
+
+            /* Initialize highest prod neighbor */
+            if (highestHeurDir == Direction.STILL)
+                highestHeurDir = dir
+
+            /* Note higher production neighbors */
+            if (heuristic(neighLoc) > heuristic(gameMap.getLocation(loc,
+                    highestHeurDir)))
+                highestHeurDir = dir
+
+        }
+    }
+
+
+    /* Attack weaker neighbours */
+    if (border && gameMap.getSite(loc, highestHeurDir).strength < site.strength)
+        return highestHeurDir
+
+    /* Wait for production */
+    if (site.strength < site.production * PROD_MULTIPLIER || site.production == 0)
+        return Direction.STILL
+
+    /* If inside and not producing, move */
+    if (!border)
+        return nearestEnemyDir(loc)
+
+    /* Fail-safe */
+    logger.warning(String
+            .format("Turn %d: [%d, %d] Couldn't come up with a move. Standing still.",
+                    turnCounter, loc.x, loc.y))
+    return Direction.STILL
+
+}
+
+fun qualityOfTile(site: Site): Float {
+    return (if (site.strength == 0)
+        site.production / site.strength
+    else
+        site.production).toFloat()
+}
+
+
+fun initMap() {
+    val gameMap = gameMap!!
+
+    processedMap = Array(gameMap.height) { Array(gameMap.width) { FloatArray(PROCESSING_PROPERTIES) } }
+    qualityMap = Array(gameMap.height) { Array(gameMap.width) { FloatArray(PROCESSING_PROPERTIES) } }
+
+    val processedMap = processedMap!!
+    val qualityMap = qualityMap!!
+
+    for (x in 0..gameMap.height - 1) {
+        for (y in 0..gameMap.width - 1) {
+
+            /* Process one tile */
+            val site = gameMap.getLocation(x, y).site
+
+            qualityMap[x][y][0] = qualityOfTile(site)
+            qualityMap[x][y][1] = if (site.owner != 0 && site.owner != myId)
+                site.strength.toFloat()
+            else    0f
+
+        }
+    }
+    /* Build map of blurred sites */
+    for (x in 0..gameMap.height - 1) {
+        for (y in 0..gameMap.width - 1) {
+
+            var sumQ = 0f
+            var sumA = 0f
+
+            for (x1 in (BLUR_DIMENSION - 1) / 2 * -1..BLUR_DIMENSION - 1) {
+                for (y1 in (BLUR_DIMENSION - 1) / 2 * -1..BLUR_DIMENSION - 1) {
+
+                    // Translate to real location
+                    val x2 = if (x1 + x < 0)
+                        gameMap.height + x1 - 1
+                    else if (x + x1 >= gameMap.height)
+                        x + x1 - gameMap.height
+                    else
+                        x + x1
+
+                    val y2 = if (y1 + y < 0)
+                        gameMap.width + y1 - 1
+                    else if (y + y1 >= gameMap.width)
+                        y + y1 - gameMap.width
+                    else
+                        y + y1
+
+                    val weight:Float = if (x1 == 0 && y1 == 0)
+                        1f
+                    else
+                        1f / (1 + Math.abs(x1) + Math
+                                .abs(y1)).toFloat()
+
+                    sumQ += qualityMap[x2][y2][0] * weight
+                    sumA += qualityMap[x2][y2][1] * weight
+
+                    logger.finer(String.format("Blur: [%d,%d] considering [%d,%d] with %f.3 weight and %f.5 sumQ",
+                    										x, y, x2, y2, weight, sumQ))
+
+                }
+            }
+
+            // logger.info(String.format(
+            // "processing! [%d, %d] Q: %f | A: %f \n\n", x, y, sumQ,
+            // sumA));
+            processedMap[x][y][0] = sumQ
+            processedMap[x][y][1] = sumA
+        }
+    }
+
+    /* Use those maps to find sectors of interest */
+    regionMap = Array(SECTORS_BY_SIDE) { Array(SECTORS_BY_SIDE) { FloatArray(PROCESSING_PROPERTIES) } }
+
+    val regionMap = regionMap!!
+
+    val sectorEdge = gameMap.height / SECTORS_BY_SIDE
+    val currentMostSector = FloatArray(6) // 0 x | 1 y | 2 quality | 3 x
+    // [army] | 4 y [army] | 5
+    // army
+
+    for (i in 0..SECTORS_BY_SIDE - 1) {
+        for (j in 0..SECTORS_BY_SIDE - 1) {
+
+            var sumQ = 0f
+            var sumA = 0f
+            for (x in 0..sectorEdge - 1) {
+                for (y in 0..sectorEdge - 1) {
+
+                    sumQ += processedMap[i * sectorEdge + x][j * sectorEdge + y][0]
+                    sumA += processedMap[i * sectorEdge + x][j * sectorEdge + y][1]
+
+
+                }
+            }
+
+            regionMap[i][j][0] = (sumQ / Math.pow(sectorEdge.toDouble(), 2.0)).toFloat()
+            regionMap[i][j][1] = (sumA / Math.pow(sectorEdge.toDouble(), 2.0)).toFloat()
+
+            logger.info("Region completed. processedMap:\n"
+                    + processedMap[i] + processedMap[i][j] + " "
+                    + processedMap[i][j][0] + " " + processedMap[5][5][0])
+            if (sumQ > currentMostSector[2]) {
+
+                logger.info("Current most sector [2] is " + currentMostSector[2])
+                currentMostSector[0] = i.toFloat()
+                currentMostSector[1] = j.toFloat()
+                currentMostSector[2] = sumQ
+
+            }
+
+            if (sumA > currentMostSector[5]) {
+                currentMostSector[3] = i.toFloat()
+                currentMostSector[4] = j.toFloat()
+                currentMostSector[5] = sumA
+
+            }
+
+        }
+    }
+
+    logger.info(String
+            .format("Analysis complete. Most interesting regions:\n\t[%.0f,%.0f] Quality: %f\n\t[%f.0,%.0f] Army: %f",
+                    currentMostSector[0], currentMostSector[1],
+                    currentMostSector[2], currentMostSector[3],
+                    currentMostSector[4], currentMostSector[5]))
 }
 
 fun initLog() = try {
@@ -124,7 +373,7 @@ fun initLog() = try {
     logger.level = LOGGING_LEVEL
     logger.info("Link starto!")
 
-    val turnTimes = mutableListOf<Int>()
+    turnTimes = mutableListOf<Int>()
 } catch (e:SecurityException) {
     e.printStackTrace()
 } catch (e:IOException) {
@@ -132,3 +381,19 @@ fun initLog() = try {
 }
 
 
+private fun logTurn(startTime: Long) {
+    val turnTimes = turnTimes!!
+    turnTimes.add((System.currentTimeMillis() - startTime).toInt())
+
+    /* Every 50 turns, display detailed output */
+    if (turnCounter % 50 === 0) {
+        val logMsg = StringBuilder()
+        logMsg.append("Turn times so far: ")
+
+        for (i in 0..turnCounter - 1) {
+            logMsg.append(String.format("%dms, ", turnTimes.get(i)))
+        }
+        logger.info(logMsg.toString())
+    }
+
+}
