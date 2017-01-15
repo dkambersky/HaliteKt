baea2dd3b3f706c0e1117947447800b029946842
David Kamberský <david.kambersky@gmail.com>
2017-01-15 12:43:19 +0000

    Checkpoint #1 working bot

--- KotlinBot/src/MyBot.kt
+++ KotlinBot/src/MyBot.kt
@@ -11,17 +11,16 @@ fun main(args: Array<String>) {
 
     while (true) {
         val moves = mutableListOf<Move>()
-
         Networking.updateFrame(gameMap)
 
-        for (y in 0..gameMap.height) {
-            for (x in 0..gameMap.width) {
+        for (y in 0..gameMap.height-1) {
+            for (x in 0..gameMap.width-1) {
                 val location = gameMap.getLocation(x, y)
                 val site = location.site
 
-                if (site.owner == myId)
-                    moves.add(Move(location, Direction.randomDirection()))
+                if (site.owner == myId) moves.add(Move(location, Direction.randomDirection()))
             }
         }
+        Networking.sendFrame(moves)
     }
 }
